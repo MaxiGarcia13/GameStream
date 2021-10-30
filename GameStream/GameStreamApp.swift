b23ec1500380660e341f920785ad9e3f8516ef1f
Maximiliano Garcia <maxig8@hotmail.com>
2021-10-30 15:46:02 +0200

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Maxi Garcia on 30/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
